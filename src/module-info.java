64a8a6c207371516c164e8a2e39a21a2c039a97f
Baudson Dylan <dylan.baudson1@etu.univ-lorraine.fr>
2024-01-10 14:23:09 +0100

    test

--- src/module-info.java
+++ src/module-info.java
@@ -4,6 +4,7 @@ module groupes_sae_s3a_baudson_colson_pedretti_weier {
     requires javafx.base;
     requires java.desktop;
     requires javafx.swing;
+    requires junit;
 
     exports main;
     exports main.observateur;
---
+++ src/test/TestListe.java
@@ -0,0 +1,12 @@
+package test;
+
+import main.Liste;
+import org.junit.Test;
+
+public class TestListe {
+
+    @Test
+    public void ajouterTache() {
+        Liste liste = new Liste("Liste");
+    }
+}
