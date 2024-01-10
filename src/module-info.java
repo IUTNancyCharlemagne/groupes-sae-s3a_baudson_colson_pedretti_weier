f56ff5e6c139423422f1e2479ef5fa3cd8b603b3
Baudson Dylan <dylan.baudson1@etu.univ-lorraine.fr>
2024-01-10 14:07:51 +0100

    test

--- src/module-info.java
+++ src/module-info.java
@@ -2,9 +2,9 @@ module groupes_sae_s3a_baudson_colson_pedretti_weier {
 
     requires javafx.controls;
     requires javafx.base;
-    requires org.junit.jupiter.api;
     requires java.desktop;
     requires javafx.swing;
+    requires org.junit.jupiter.api;
 
     exports main;
     exports main.observateur;
