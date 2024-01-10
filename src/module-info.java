9b83187612ec75d7a9847dd750fd1a215da6397e
Baudson Dylan <dylan.baudson1@etu.univ-lorraine.fr>
2024-01-10 10:39:44 +0100

    soustachevide

--- src/module-info.java
+++ src/module-info.java
@@ -3,7 +3,6 @@ module groupes_sae_s3a_baudson_colson_pedretti_weier {
     requires javafx.controls;
     requires javafx.base;
     requires org.junit.jupiter.api;
-    requires junit;
 
     exports main;
     exports main.observateur;
